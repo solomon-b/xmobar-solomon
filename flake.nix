271ce374f7f686bedbab0809f37743e74ca757cf
Solomon Bothwell <ssbothwell@gmail.com>
2021-09-14 22:55:07 -0700

    still trying to fix this flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,20 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {inherit system;};
+      xmobar-solomon = pkgs.haskellPackages.callCabal2nix "xmobar" (./.);
+    in {
     defaultPackage.x86_64-linux =
-      let pkgs = import nixpkgs { system = "x86_64-linux"; };
-          xmobar-solomon = pkgs.haskellPackages.callCabal2nix "xmobar-solomon" (./.);
-      in pkgs.haskellPackages.callPackage xmobar-solomon { };
+      pkgs.haskellPackages.callPackage xmobar-solomon { };
 
     overlay = final: prev: {
       haskellPackages = prev.haskellPackages.override (old: {
         overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
         (hself: hsuper: {
-          xmobar = pkgs.haskellPackages.callCabal2nix "xmobar" (./.);
+          xmobar = xmobar-solomon;
         });
       });
     };
