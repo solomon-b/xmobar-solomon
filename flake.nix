36a760ea2dc4ec0a239b438340d53abab5564d71
solomon <ssbothwell@gmail.com>
2024-01-04 13:53:16 -0800

    Updates to nixpkgs 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My XMobar Wrapper";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-23.05;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-23.11;
     flake-utils.url = github:numtide/flake-utils;
   };
 
